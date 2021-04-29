fdf52329aaecbfa615115ad663bc0ece949cf8e6
Kunal1358 <kunalkohli8054@gmail.com>
2021-04-29 09:35:03 +0530

    initial commit

---
+++ src/main/java/com/Chitkara/DataCollectionFormApplication.java
@@ -0,0 +1,13 @@
+package com.Chitkara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCollectionFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCollectionFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Chitkara/DataCollectionFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Chitkara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCollectionFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
